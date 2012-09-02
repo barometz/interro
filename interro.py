fe60b0ecfa6e6f17aa6ffb1c1346ae0676995533
Dominic <dominic@baudvine.net>
2012-09-02 19:37:35 +0200

    Some documentation

--- interro.py
+++ interro.py
@@ -3,8 +3,29 @@
 # Copyright (c) 2012 Dominic van Berkel.  See LICENSE for details.
 #
 
-
 class InterroQ:
+    """Base class for Interro questions.  
+
+    InterroQ has some infrastructure for validation checks, type checks that
+    more specific question types can use and answer-dependant branching.
+
+    Usage: 
+    Create an instance with whatever parameters you like.  When you've got an
+    answer, run it through .validate() to make sure it's of the right format.
+    When that's done it can be stored with .store(), which will automatically
+    run the value through .parse() in order to convert it to an appropriate
+    type, format or otherwise.  Mostly it's expected that values thrown at 
+    InterroQ are all strings.
+
+    Interesting functions to override: 
+    - parse: Takes a value and returns a format-appropriate representiation.
+      See YesNoQ for an example.
+    - add_typechecks: This is where subclasses can specify their type checks.
+    - preprocess: General processing that should be done first, such as
+      str.strip() and .lower()
+
+    """
+
     name = ''
     question = None
     message = None
@@ -18,6 +39,21 @@ class InterroQ:
 
     def __init__(self, name, question=None, message=None, default=None, 
                  onanswer=None, validation=None, confirm=False):
+        """Create an InterroQ instance.
+
+        Parameters:
+        - name: name of the question.  Can be used to identify the information.
+        - question: The question to be asked.  
+        - message: A message that will be shown when a question comes up for the
+          first time.
+        - default: The name of the question that should come after by default.
+        - onanswer: a dictionary of {value: InterroQ}.  If the stored and
+          parsed answer matches a value, this indicates what question to pull
+          up next.
+        - validation: A list of (method: error) tuples. 
+        - confirm: Indicates whether the question requires confirmation.
+
+        """
         self.name = name
         self.question = question
         self.message = message
@@ -28,12 +64,30 @@ class InterroQ:
         self.add_typechecks()
 
     def add_typechecks(self, *args):
+        """Add typechecks.
+
+        The advice for subclasses is to construct a list of (method, error)
+        and throw that at super() together with *args.
+
+        """
         self.type_validation = args
 
     def preprocess(self, value):
+        """Cleanup and essential formatting of input.
+
+        Should be called by validate() and parse() before doing anything with
+        the input.
+
+        """
         return value
 
     def validate(self, value):
+        """Runs the value through all known tests.
+
+        Starts with type validation, then moves on to user-configured 
+        requirements.
+
+        """
         value = self.preprocess(value)
         for test, error in self.type_validation:
             if not test(value):
@@ -47,12 +101,22 @@ class InterroQ:
         return True
 
     def parse(self, value):
+        """Convert the value to someting appropriate for the question type.
+
+        For example, YesNoQ turns it into a boolean, while a NumberQ might
+        change it into an integer or float.  It's okay if this raises an
+        exception on failure, as the type-specific validation checks should
+        already make sure that it can be converted in the first place.
+
+        """
         return self.preprocess(value)
 
     def store(self, value):
+        """Parse and store the input."""
         self.value = self.parse(value)
 
     def nextq(self):
+        """Find out what the next question is based on the current value."""
         if self.onanswer and self.value in self.onanswer:
             return self.onanswer[self.value]
         else:
@@ -60,6 +124,13 @@ class InterroQ:
             
 
 class StringQ(InterroQ):
+    """Text question class.
+
+    Does very little, other than - if desired - making sure the input isn't
+    empty and trimming the sides.
+
+    """
+
     empty_allowed = False
 
     def __init__(self, name, empty_allowed=False, **kwargs):
@@ -84,6 +155,8 @@ class StringQ(InterroQ):
 
 
 class YesNoQ(InterroQ):
+    """Yes/No-question class."""
+
     def add_typechecks(self, *args):
         yesno = (lambda x: x in ['y', 'yes', 'n', 'no'], 'Please enter yes or no.')
         super().add_typechecks(yesno, *args)
@@ -100,6 +173,21 @@ class YesNoQ(InterroQ):
 
 
 class Interro:
+    """Core "interrogation" class.
+
+    Keeps track of the questions in what is essentially a singly-linked list.
+    Spits out questions, descriptive messages and errors, passes answers on to
+    the InterroQ objects and asks for confirmation as needed.
+
+    Usage:
+    After instantiation InterroQ instances can be passed to add() which will
+    add them to the list.  After start() is called, messages will be
+    chronologically filled with messages from questions as they roll in.
+    Submit answers through answer() as long as complete isn't True, then call
+    results() to get a dictionary of results.  Terminates when an InterroQ is
+    processed that doesn't actually have a question attached to it.
+
+    """
     tree = None
     current = None
     messages = None
@@ -114,6 +202,7 @@ class Interro:
         self._start = start
 
     def results(self):
+        """Get a dictionary of {name: value} with the results so far"""
         res = {}
         for q in self.tree.values():
             if q.value is not None:
@@ -121,12 +210,22 @@ class Interro:
         return res
 
     def add(self, datum):
+        """Add an InterroQ instance to the list."""
         self.tree[datum.name] = datum
 
     def start(self):
+        """Start pulling questions."""
         self._nextdatum(goto=self._start)
 
     def answer(self, value):
+        """Process an answer.
+
+        If we're waiting for confirmation, handle that and move to the next
+        question if the answer is yes.  Otherwise, throw the answer at the
+        current InterroQ to see if it validates, then store it and maybe ask
+        for confirmation.
+
+        """
         cur = self.current
         if self._pendingconfirmation:
             value = value.strip().lower()
@@ -136,6 +235,7 @@ class Interro:
                 self.messages.append(cur.question)
             self._pendingconfirmation = False
         else:
+            # Throw the answer at the current question
             if cur.validate(value):
                 cur.store(value)
                 if cur.confirm:
@@ -149,6 +249,12 @@ class Interro:
                 self.messages.append(cur.question)
 
     def _nextdatum(self, goto=None):
+        """Move to the next question, if any.
+
+        Sets self.complete to true if the new InterroQ doesn't actually have a
+        question, but does add its message - if any - to the message queue.
+
+        """
         if self.current:
             nextq = self.current.nextq()
         else:
